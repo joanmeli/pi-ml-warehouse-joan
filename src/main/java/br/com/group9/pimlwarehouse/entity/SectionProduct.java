80ebb1802ee99dc7e3f3f56436b536fc4f87858f
Guilherme da Silva <guilherme.pereira@mercadolivre.com>
2022-04-26 15:48:00 -0300

    feature: creating SectionProduct structure

--- src/main/java/br/com/group9/pimlwarehouse/entity/SectionProduct.java
+++ src/main/java/br/com/group9/pimlwarehouse/entity/SectionProduct.java
@@ -1,4 +1,29 @@
 package br.com.group9.pimlwarehouse.entity;
 
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+@Table(name = "sectionProduct")
 public class SectionProduct {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "section_product_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "section_id")
+    private Section sectionId;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product productId;
 }
