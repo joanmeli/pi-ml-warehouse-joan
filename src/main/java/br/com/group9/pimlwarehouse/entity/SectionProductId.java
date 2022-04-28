f6940ea135ff6872e53ab8e8b15a44482a4ad8e1
Vinicius Clemente Brito <vinicius.brito@mercadolivre.com>
2022-04-28 10:58:45 -0300

    refact: excluding unused entity

--- src/main/java/br/com/group9/pimlwarehouse/entity/SectionProductId.java
+++
@@ -1,8 +0,0 @@
-package br.com.group9.pimlwarehouse.entity;
-
-import java.io.Serializable;
-
-public class SectionProductId implements Serializable {
-    private Section sectionId;
-    private Long productId;
-}
