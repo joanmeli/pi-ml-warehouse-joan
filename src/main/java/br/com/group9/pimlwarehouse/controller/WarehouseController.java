3d0e540c66b6471c65482a25ed8bd70adb2665a7
Guilherme da Silva <guilherme.pereira@mercadolivre.com>
2022-05-02 14:06:28 -0300

    feat: inserting javadocs on warehouse controller

--- src/main/java/br/com/group9/pimlwarehouse/controller/WarehouseController.java
+++ src/main/java/br/com/group9/pimlwarehouse/controller/WarehouseController.java
@@ -24,6 +24,14 @@ public class WarehouseController extends APIController{
         this.warehouseService = warehouseService;
     }
 
+    /**
+     * POST method for create a new Warehouse
+     * @param warehouseDTO receives a valid WarehouseDTO in body of the requisition
+     * @param uriBuilder injeção utilizada pelo spring para declarar para o spring enviar o location
+     * @return the URI of warehouse on header location, the entity response with status code "201-Created" and
+     * the WarehouseDTO created as result
+     */
+
     @PostMapping(BASE_PATH)
     public ResponseEntity<WarehouseDTO> createWarehouse(@Valid @RequestBody WarehouseDTO warehouseDTO, UriComponentsBuilder uriBuilder) {
         Warehouse mappedWarehouse = warehouseDTO.map();
@@ -36,17 +44,38 @@ public class WarehouseController extends APIController{
         return ResponseEntity.created(uri).body(resultWarehouse);
     }
 
+    /**
+     * GET method to search a existing warehouse by id
+     * @param warehouseId receives a Long id of warehouse in the path variable
+     * @return the Warehouse registered with the id informed,
+     * If there is no warehouse with the given id, it returns "WAREHOUSE_NOT_FOUND"
+     */
+
     @GetMapping(BASE_PATH + "/{warehouseId}")
     public ResponseEntity<WarehouseDTO> findWarehouse(@PathVariable(name = "warehouseId") Long warehouseId) {
         Warehouse foundWarehouse = this.warehouseService.findById(warehouseId);
         return ResponseEntity.ok(WarehouseDTO.map(foundWarehouse));
     }
 
+    /**
+     * GET method to search a product by id inside a warehouse
+     * @param productId receives a Long id of product in the path variable
+     * @return the warehouses where contains the product informed,
+     * if there is no product with informed id on any warehouses, it return "404-Not Found"
+     */
+
     @GetMapping("/fresh-products/warehouse/{productId}")
     public ResponseEntity<ProductWarehouseDTO> findProductInWarehouse(@PathVariable Long productId) {
         Map<Long, Integer> product = warehouseService.getAllWarehousesByProduct(productId);
         return ResponseEntity.ok(ProductWarehouseDTO.convert(productId, WarehouseProductDTO.convert(product)));
     }
+
+    /**
+     * GET method to find and list all of products where it appear in stock
+     * @param productIds receives a list of productsIds where the list type is Long
+     * @param orderBy receives the type of sorting that will be performed
+     * @return the location of the products ordered by Batch number, current quantity and due date
+     */
 
     @GetMapping("/fresh-products/list")
     public ResponseEntity<List<SectionBatchStockDTO>> findProductsInStock(
