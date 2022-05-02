cd4aed69b8b0751b52a5aa63ca4ce764ed370f33
Vinicius Clemente Brito <vinicius.brito@mercadolivre.com>
2022-05-02 19:10:49 -0300

    refact: changed controller to BatchStockController for fresh products routes

--- src/main/java/br/com/group9/pimlwarehouse/controller/WarehouseController.java
+++ src/main/java/br/com/group9/pimlwarehouse/controller/WarehouseController.java
@@ -1,31 +1,22 @@
 package br.com.group9.pimlwarehouse.controller;
 
-import br.com.group9.pimlwarehouse.dto.*;
 import br.com.group9.pimlwarehouse.dto.WarehouseDTO;
-import br.com.group9.pimlwarehouse.entity.BatchStock;
 import br.com.group9.pimlwarehouse.entity.Warehouse;
-import br.com.group9.pimlwarehouse.service.BatchStockService;
 import br.com.group9.pimlwarehouse.service.WarehouseService;
-import br.com.group9.pimlwarehouse.util.batch_stock_order.OrderBatchStockEnum;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
-import java.util.List;
-import java.util.Map;
-import java.util.List;
 
 @RestController
 public class WarehouseController extends APIController{
     private static final String BASE_PATH = "/warehouse";
     private WarehouseService warehouseService;
-    private BatchStockService batchStockService;
 
-    public WarehouseController(WarehouseService warehouseService, BatchStockService batchStockService) {
+    public WarehouseController(WarehouseService warehouseService) {
         this.warehouseService = warehouseService;
-        this.batchStockService = batchStockService;
     }
 
     @PostMapping(BASE_PATH)
@@ -44,26 +35,5 @@ public class WarehouseController extends APIController{
     public ResponseEntity<WarehouseDTO> findWarehouse(@PathVariable(name = "warehouseId") Long warehouseId) {
         Warehouse foundWarehouse = this.warehouseService.findById(warehouseId);
         return ResponseEntity.ok(WarehouseDTO.map(foundWarehouse));
-    }
-
-    @GetMapping("/fresh-products/warehouse/{productId}")
-    public ResponseEntity<ProductWarehouseDTO> findProductInWarehouse(@PathVariable Long productId) {
-        Map<Long, Integer> product = warehouseService.getAllWarehousesByProduct(productId);
-        return ResponseEntity.ok(ProductWarehouseDTO.convert(productId, WarehouseProductDTO.convert(product)));
-    }
-
-    @GetMapping("/fresh-products/list")
-    public ResponseEntity<List<SectionBatchStockDTO>> findProductsInStock(
-            @RequestParam(name = "products", required = false, defaultValue = "") List<Long> productIds,
-            @RequestParam(name = "order_by", required = false, defaultValue = "DEFAULT") OrderBatchStockEnum orderBy
-    ) {
-        Map<Long, List<BatchStock>> foundBatchStocks = this.warehouseService.getProductsInStockByIds(productIds, orderBy);
-        return ResponseEntity.ok(SectionBatchStockDTO.map(foundBatchStocks));
-    }
-
-    @PostMapping("/fresh-products/")
-    public ResponseEntity<List<BatchStockDTO>> withdrawStock(@RequestBody List<ProductBatchStockDTO> products) {
-        List<BatchStock> batchStocks = this.batchStockService.withdrawStockByProductId(ProductBatchStockDTO.convert(products));
-        return ResponseEntity.ok(BatchStockDTO.convert(batchStocks));
     }
 }
