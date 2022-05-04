4bc946ac177ffbb0c444e273f9ce8ecda6578b0d
Vinicius Clemente Brito <vinicius.brito@mercadolivre.com>
2022-05-04 12:41:38 -0300

    refact: refactored getProductsInfo to make only 1 api call to products api

--- src/main/java/br/com/group9/pimlwarehouse/service/ProductAPIService.java
+++ src/main/java/br/com/group9/pimlwarehouse/service/ProductAPIService.java
@@ -9,13 +9,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class ProductAPIService {
-    private static final String PRODUCT_API_URI = "http://products:8081";
+    private static final String PRODUCT_API_URI = "http://localhost:8081";
     private static final String PRODUCTS_RESOURCE = "/fresh-products/v1";
     private final RestTemplate restTemplate;
 
@@ -27,7 +29,7 @@ public class ProductAPIService {
 
     /**
      * Fetch for product by Id in the products API.
-     * @param id receives a productId to associate a section.
+     * @param id receives a productId to search for.
      * @return will perform a communication with the products API and returns the result.
      * If an exception occurs, returns "PRODUCT_NOT_FOUND".
      */
@@ -38,15 +40,24 @@ public class ProductAPIService {
         return result.getBody();
     }
 
-    public ProductDTO fetchProductsById(Map<String, List<Long>> ids) {
-        String resourceURI = PRODUCT_API_URI.concat(PRODUCTS_RESOURCE).concat("/");
-        // TODO: 27/04/22 Create custom validations on Response for Product API.
-        try {
-            ResponseEntity<ProductDTO> result = restTemplate.getForEntity(resourceURI, ProductDTO.class );
-            return result.getBody();
-        } catch (RuntimeException ex) {
-            throw new ProductNotFoundException("PRODUCT_NOT_FOUND");
-        }
+    /**
+     * Fetch several products by Id List in the products API.
+     * @param ids receives a List<Long> of productIds to search for.
+     * @return will perform a communication with the products API and returns a Map of the result.
+     * If an exception occurs, returns "PRODUCT_NOT_FOUND".
+     */
+    public Map<Long, ProductDTO> fetchProductsById(List<Long> ids) {
+        String concatenatedIds = ids.stream().map(String::valueOf).collect(Collectors.joining(","));
+        String resourceURI = PRODUCT_API_URI.concat(PRODUCTS_RESOURCE).concat("?products=").concat(concatenatedIds);
+        ResponseEntity<ProductDTO[]> result = restTemplate.getForEntity(resourceURI, ProductDTO[].class );
+        return ids.stream()
+                .map(e -> Map.entry(
+                        e,
+                        Arrays.stream(result.getBody()).filter(p -> p.getId().equals(e))
+                                .findFirst()
+                                .orElseThrow(() -> new ProductNotFoundException("PRODUCT_NOT_FOUND: ".concat(e.toString())))
+                ))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (a, b) -> b));
     }
 
     /**
@@ -55,8 +66,11 @@ public class ProductAPIService {
      * @return returns a list of products from Product API.
      */
     public List<Map<ProductDTO, BatchStockDTO>> getProductInfo(List<BatchStockDTO> batchStockList) {
+        List<Long> idList = batchStockList.stream().map(b -> b.getProductId()).collect(Collectors.toList());
+        Map<Long, ProductDTO> foundProductsMap = fetchProductsById(idList);
+
         return batchStockList.stream().map(batchStockDTO ->
-                Map.of(fetchProductById(batchStockDTO.getProductId()), batchStockDTO)
+                    Map.of(foundProductsMap.get(batchStockDTO.getProductId()), batchStockDTO)
         ).collect(Collectors.toList());
     }
 }
