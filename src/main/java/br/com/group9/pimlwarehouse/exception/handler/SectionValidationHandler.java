47783dbb6b7f65eaabcde32e122640ccab19662d
Guilherme da Silva <guilherme.pereira@mercadolivre.com>
2022-05-04 14:50:44 -0300

    adjustment: sending correct status code on product not found

--- src/main/java/br/com/group9/pimlwarehouse/exception/handler/SectionValidationHandler.java
+++ src/main/java/br/com/group9/pimlwarehouse/exception/handler/SectionValidationHandler.java
@@ -5,6 +5,7 @@ import br.com.group9.pimlwarehouse.exception.ProductDoesNotMatchSectionException
 import br.com.group9.pimlwarehouse.exception.ProductNotFoundException;
 import br.com.group9.pimlwarehouse.exception.SectionNotFoundException;
 import br.com.group9.pimlwarehouse.exception.SectionProductNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,7 +16,7 @@ public class SectionValidationHandler {
     @ExceptionHandler(ProductNotFoundException.class)
     protected ResponseEntity<Object> handleProductNotFoundException(ProductNotFoundException exception) {
         ErrorMessageDTO error = new ErrorMessageDTO(exception.getMessage());
-        return ResponseEntity.badRequest().body(error);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
     @ExceptionHandler(SectionNotFoundException.class)
     protected ResponseEntity<Object> handleSectionNotFoundException(SectionNotFoundException exception) {
